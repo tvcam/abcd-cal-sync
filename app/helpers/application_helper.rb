06d4b963e0697414ddf2fccdbcf60290d377e643
Vibol Teav <vibolteav@gmail.com>
2022-07-10 04:38:40 +0700

    Render flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def render_flash_message
+    if flash.present?
+      content_tag(:div, class: 'highlight') do
+        flash.each do |type, message|
+          concat content_tag(:pre, message)
+        end
+      end
+    end
+  end
 end
