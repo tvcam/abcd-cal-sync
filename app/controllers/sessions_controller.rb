dc4b794c5bcc3180bd4ceb4e5b77da62dad18316
Vibol Teav <vibolteav@gmail.com>
2022-07-10 08:18:37 +0700

    Refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,19 +8,12 @@ class SessionsController < ApplicationController
 
   def callback
     client = Signet::OAuth2::Client.new(CalendarSync.oauth2_client_options)
-    validator = GoogleIDToken::Validator.new
     client.code = params[:code]
     response = client.fetch_access_token!
 
     begin
-      payload = validator.check(response['id_token'], Rails.application.credentials.google_client_id)
-      user = User.find_or_create_by(email: payload['email'], google_user_id: payload['sub'])
-      user.update_columns(access_token: response['access_token'], refresh_token: response['refresh_token'])
-      session[:user_id] = user.id
-
-      sync_service = CalendarSync.new(user)
-      sync_service.sync_today
-
+      user = setup_user(response)
+      CalendarSync.sync_today(user)
 
       redirect_to root_path, notice: 'Authorize success'
     rescue GoogleIDToken::ValidationError => e
@@ -32,5 +25,17 @@ class SessionsController < ApplicationController
   def destroy
     reset_session
     redirect_to root_path, notice: 'Log out success'
+  end
+
+  private
+
+  def setup_user(access_token_response)
+    validator = GoogleIDToken::Validator.new
+    payload = validator.check(access_token_response['id_token'], Rails.application.credentials.google_client_id)
+    user = User.find_or_create_by(email: payload['email'], google_user_id: payload['sub'])
+    user.update_columns(access_token: access_token_response['access_token'], refresh_token: access_token_response['refresh_token'])
+    session[:user_id] = user.id
+
+    user
   end
 end
--- app/services/calendar_sync.rb
+++ app/services/calendar_sync.rb
@@ -1,20 +1,27 @@
 class CalendarSync
   class Unauthorized < ::StandardError; end
 
-  CALENDARS_ENDPOINT = 'https://www.googleapis.com/calendar/v3/users/me/calendarList'
-  EVENTS_ENDPOINT = 'https://www.googleapis.com/calendar/v3/calendars/:calendar_id/events'
+  CALENDARS_ENDPOINT = 'https://www.googleapis.com/calendar/v3/users/me/calendarList'.freeze
+  EVENTS_ENDPOINT = 'https://www.googleapis.com/calendar/v3/calendars/:calendar_id/events'.freeze
 
   attr_reader :user, :start_at, :end_at
 
-  def self.oauth2_client_options
-    {
-      client_id: Rails.application.credentials.google_client_id,
-      client_secret: Rails.application.credentials.google_client_secret,
-      authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
-      token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
-      scope: [Google::Apis::CalendarV3::AUTH_CALENDAR_EVENTS_READONLY, Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY, 'https://www.googleapis.com/auth/userinfo.email'],
-      redirect_uri: 'http://localhost:3000/google_sign_in/callback'
-    }
+  class << self
+    def oauth2_client_options
+      {
+        client_id: Rails.application.credentials.google_client_id,
+        client_secret: Rails.application.credentials.google_client_secret,
+        authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+        token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+        scope: [Google::Apis::CalendarV3::AUTH_CALENDAR_EVENTS_READONLY, Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY, 'https://www.googleapis.com/auth/userinfo.email'],
+        redirect_uri: 'http://localhost:3000/google_sign_in/callback'
+      }
+    end
+
+    def sync_today(user)
+      service = new(user)
+      service.sync_today
+    end
   end
 
   def initialize(user)
@@ -35,40 +42,35 @@ class CalendarSync
       calendar.update_columns(primary: google_calendar['primary'], summary: google_calendar['summary'])
 
       events = fetch_events(calendar.google_id)
-      events.each do |google_event|
-        event = calendar.events.find_or_create_by(google_id: google_event['id'])
-
-        start_date = google_event.dig('start', 'dateTime')
-        start_date ||= google_event.dig('start', 'date')
-
-        end_date = google_event.dig('end', 'dateTime')
-        end_date ||= google_event.dig('end', 'date')
-
-        event.update_columns(
-          attendees: google_event['attendees'],
-          creator: google_event['creator'],
-          event_end: google_event['end'],
-          event_start: google_event['start'],
-          description: google_event['description'],
-          status: google_event['status'],
-          summary: google_event['summary'],
-          start_at: start_date,
-          end_at: end_date
-        )
-      end
+      events.each { |google_event| upsert_event(calendar, google_event) }
     end
   end
 
   private
 
   def fetch_calendars
-    @calendar ||= make_request(CALENDARS_ENDPOINT)
+    @fetch_calendars ||= make_request(CALENDARS_ENDPOINT)
   end
 
   def fetch_events(calendar_id)
     events_endpoint = EVENTS_ENDPOINT.gsub(':calendar_id', calendar_id)
-    # events_endpoint = "#{events_endpoint}?timeMax=#{end_at}&timeMin=#{start_at}"
+    events_endpoint = "#{events_endpoint}?timeMax=#{end_at}&timeMin=#{start_at}"
     make_request(events_endpoint)
+  end
+
+  def upsert_event(calendar, google_event)
+    event = calendar.events.find_or_create_by(google_id: google_event['id'])
+    event.update_columns(
+      attendees: google_event['attendees'],
+      creator: google_event['creator'],
+      event_end: google_event['end'],
+      event_start: google_event['start'],
+      description: google_event['description'],
+      status: google_event['status'],
+      summary: google_event['summary'],
+      start_at: (google_event.dig('start', 'dateTime') || google_event.dig('start', 'date')),
+      end_at: (google_event.dig('end', 'dateTime') || google_event.dig('end', 'date'))
+    )
   end
 
   def make_request(endpoint)
@@ -80,7 +82,7 @@ class CalendarSync
     request['authorization'] = "Bearer #{user.access_token}"
     response = http.request(request)
 
-    raise Unauthorized.new(response.message) if response.message == 'Unauthorized'
+    raise Unauthorized, response.message if response.message == 'Unauthorized'
 
     return [] if response.message == 'Not Found'
 
