b4dd0390f767ce208c7ad8ab61f6a0fc76eceb03
Vibol Teav <vibolteav@gmail.com>
2022-07-10 09:53:47 +0700

    Add ability for user to manually sync calendars

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,9 @@ class HomeController < ApplicationController
   def index
     @calendars = current_user.calendars.order(primary: :desc, summary: :asc).limit(50)
   end
+
+  def sync_calendar
+    CalendarSync.sync_today(current_user)
+    redirect_to root_path, notice: 'Sync success'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   delete :sign_out, to: 'sessions#destroy'
+
+  get 'sync_calendar', to: 'home#sync_calendar'
   get 'authorize', to: 'sessions#new'
   get 'google_sign_in/callback', to: 'sessions#callback'
 end
