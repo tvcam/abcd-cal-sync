84cdcf5fffaafec759bf25e5ae9f05b51a251427
Vibol Teav <vibolteav@gmail.com>
2022-07-10 10:17:42 +0700

    Add basic specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { is_expected.to have_many(:calendars) }
+    it { is_expected.to have_many(:events).through(:calendars) }
+  end
 end
